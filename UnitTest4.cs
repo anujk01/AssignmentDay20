c02fcbd18f9bc88f70cd2f6cebcfd23cc9293ed7
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-03-08 23:28:50 +0530

    MoodAnalyserProblems

---
+++ UnitTest4.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AssignmentDay20
+{
+    [TestClass]
+    public class UnitTest4
+    {
+        [TestInitialize]
+        public void SetUp()
+        {
+            //moodAnalyser = new MoodAnalyser(message);
+        }
+        [TestMethod]
+        public void TestMethodForHappyMood()
+
+        {
+            string expected = "happy";
+            UC4_UsingReflection moodAnalyser = new UC4_UsingReflection("I am in happy Mood");
+            string actual = moodAnalyser.AnalyzeMood();
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethodForSadMood()
+
+        {
+            string expected = "sad";
+            UC4_UsingReflection moodAnalyser = new UC4_UsingReflection("I am in sad Mood");
+            string actual = moodAnalyser.AnalyzeMood();
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethodForNullMood()
+
+        {
+            string expected = "happy";
+            UC4_UsingReflection moodAnalyser = new UC4_UsingReflection("I am in happy Mood");
+            string actual = moodAnalyser.AnalyzeMood();
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethodForCustomizedNullException()
+
+        {
+            string expected = "Mood should not be null";
+            try
+            {
+
+                UC4_UsingReflection moodAnalyser = new UC4_UsingReflection(null);
+                moodAnalyser.AnalyzeMood();
+            }
+            catch (CustomException ex)
+            {
+                Assert.AreEqual(expected, ex.Message);
+            }
+        }
+        [TestMethod]
+        public void TestMethodForCustomizedEmptyException()
+
+        {
+            string expected = "Mood should not be empty";
+            try
+            {
+
+                UC4_UsingReflection moodAnalyser = new UC4_UsingReflection(string.Empty);
+                moodAnalyser.AnalyzeMood();
+            }
+            catch (CustomException ex)
+            {
+                Assert.AreEqual(expected, ex.Message);
+            }
+
+        }
+    }
+}
