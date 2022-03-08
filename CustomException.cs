25b05bba35ed30f2b2c30ca001cc97046464aa9f
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-03-08 23:39:36 +0530

    MoodAnalyserProblems

---
+++ CustomException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssignmentDay20
+{
+    public class CustomException : Exception
+    {
+        public enum ExceptionType
+        {
+            NULL_Exception,
+            EMPTY_Exception,
+            Empty_Exception,
+            Null_Exception,
+            EMPTY_EXCEPTION
+        }
+
+        public readonly ExceptionType type;
+
+        public CustomException(ExceptionType Type, String message) : base(message)
+        {
+            this.type = Type;
+        }
+    }
+}
