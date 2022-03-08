6a047498b4e3349772c668d5d51038c2e9215a98
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-03-08 23:33:11 +0530

    MoodAnalyserProblems

---
+++ UC6.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace AssignmentDay20
+{
+    public class UC6_InvokedMethod
+    {
+        string message;
+        public UC6_InvokedMethod()
+        {
+            Console.WriteLine("Default constructor");
+        }
+        public UC6_InvokedMethod(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyzeMood()
+        {
+            try
+            {
+                if (message.Equals(string.Empty))
+                {
+                    throw new CustomException(CustomException.ExceptionType.EMPTY_EXCEPTION, "Mood should not be empty");
+                }
+                else if (message.ToLower().Contains("happy"))
+                {
+                    return "happy";
+                }
+                else
+                {
+                    return "sad";
+                }
+            }
+            catch (NullReferenceException)
+            {
+                throw new CustomException(CustomException.ExceptionType.Null_Exception, "Mood should not be null");
+            }
+        }
+    }
+}
---
+++ UnitTest6.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AssignmentDay20
+{
+    [TestClass]
+    public class UnitTest6
+    {
+        [TestMethod]
+        public void Reflection_Return_Method()
+        {
+            string expected = "happy";
+            MoodAnalyserFactory factory = new MoodAnalyserFactory();
+            string actual = factory.InvokeAnalyserMethod("happy", "AnalyzeMood");
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Reflection_Return_Invalid_Method()
+        {
+            string expected = "happy";
+            MoodAnalyserFactory factory = new MoodAnalyserFactory();
+            string actual = factory.InvokeAnalyserMethod("happy", "Analyze");
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
