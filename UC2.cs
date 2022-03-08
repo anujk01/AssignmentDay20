f0554584010525a8c97474e6b3b33fcf4f158da7
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-03-08 23:17:25 +0530

    MoodAnalyserProblems

---
+++ UC2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentDay20
+{
+    public class UC2_HandleException
+    {
+        public string message;    
+
+
+        public UC2_HandleException(string message)
+        {
+            this.message = message;
+        }
+        public string Analyzer()
+        {
+            try
+            {
+                if (this.message.ToLower().Contains("happy"))
+                {
+                    return "happy";
+                }
+                else
+                {
+                    return "sad";
+                }
+            }
+            catch
+            {
+                return "happy";
+            }
+        }
+    }
+}
---
+++ UnitTest2.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AssignmentDay20
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestInitialize]
+        public void SetUp()
+        {
+            UC2_HandleException moodanalyzer = new UC2_HandleException(string.Empty);
+        }
+        [TestMethod]
+        public void TestMethodForHappyMood()
+
+        {
+            string expected = "Happy Mood";
+            UC2_HandleException moodAnalyser = new UC2_HandleException("I am in happy Mood");
+            string actual = moodAnalyser.Analyzer();
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethodForSadMood()
+
+        {
+            string expected = "sad";
+            UC2_HandleException moodAnalyser = new UC2_HandleException("I am in sad Mood");
+            string actual = moodAnalyser.Analyzer();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
